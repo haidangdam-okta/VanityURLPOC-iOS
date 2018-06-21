330ad6accb7def343e916fcf0dd6ca9e5525fd55
Hai-Dang Dam <haidang.dam@okta.com>
2018-06-21 14:03:06 -0700

    Initial setup

--- VanityURLPOC-iOS/ViewController.m
+++ VanityURLPOC-iOS/ViewController.m
@@ -8,35 +8,87 @@
 
 #import "ViewController.h"
 #import <netdb.h>
-
+@import dnssd;
 @interface ViewController ()
 @property (weak, nonatomic) IBOutlet UITextField *editText;
-
++ (NSString *) getCName;
++ (void) setCName: (NSString *)cname;
 @end
 
 @implementation ViewController
 
-- (NSString *) canonicalAddressForHostAddress: (NSString *)hostAddess {
-    const char* hostAddress_char = [hostAddess UTF8String];
-    struct addrinfo hints, *res;
-    int retval;
-    
-    memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
-    hints.ai_flags = AI_CANONNAME;
-    
-    retval = getaddrinfo(hostAddress_char, NULL, &hints, &res);
-    if (retval == 0 && res->ai_canonname) {
-        return [NSString stringWithUTF8String:res->ai_canonname];
-    }
-    return nil;
+static NSString *cName;
++ (void) setCName:(NSString *)cname {
+    cName = cname;
 }
+
++(NSString *) getCName {
+    return cName;
+}
+
+- (void) canonicalAddressForHostAddress: (NSString* )hostAddess {
+    
+    dispatch_async(dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^{
+        DNSServiceRef serviceRef;
+        DNSServiceErrorType error;
+        error = DNSServiceQueryRecord(&serviceRef, 0, 0, "dan.sigmanetcorp.us", kDNSServiceType_CNAME,
+                                      kDNSServiceClass_IN, queryCallback, NULL);
+        if (error != kDNSServiceErr_NoError){
+            NSLog(@"DNS Service error");
+        }
+        
+        DNSServiceProcessResult(serviceRef);
+        DNSServiceRefDeallocate(serviceRef);
+    });
+    
+    
+}
+
+
+
+static void queryCallback(DNSServiceRef sdRef,
+                          DNSServiceFlags flags,
+                          uint32_t interfaceIndex,
+                          DNSServiceErrorType errorCode,
+                          const char *fullname,
+                          uint16_t rrtype,
+                          uint16_t rrclass,
+                          uint16_t rdlen,
+                          const void *rdata,
+                          uint32_t ttl,
+                          void *context) {
+    if (errorCode == kDNSServiceErr_NoError && rdlen > 1) {
+        NSMutableData *txtData = [NSMutableData dataWithCapacity:rdlen];
+        
+        
+        for (uint16_t i = 1; i < rdlen; i += 256) {
+            [txtData appendBytes:rdata + i length:MIN(rdlen - i, 256)];
+        }
+        
+        
+        
+        NSString *theTXT = [[NSString alloc] initWithBytes:txtData.bytes length:txtData.length encoding:NSUTF8StringEncoding];
+        [ViewController setCName:theTXT];
+        NSLog(@"%@", theTXT);
+    }
+}
+
+
+/*NSString *s = [[NSString alloc] initWithBytes:rdata
+ length:rdlen
+ encoding:NSUTF8StringEncoding];
+ 
+ NSLog(@"%@", s);
+ 
+ }
+ */
+
 
 - (IBAction)touchButton:(UIButton *)sender {
     NSString *userInput = self.editText.text;
-    NSString *canAddr = [self canonicalAddressForHostAddress:userInput];
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"URL POC" message:[NSString stringWithFormat:@"URL return from %@: %@", self.editText.text, canAddr] preferredStyle:UIAlertControllerStyleAlert];
-    UIAlertAction *acknowledge = [UIAlertAction actionWithTitle:@"Acknowledge" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
+    [self canonicalAddressForHostAddress:userInput];
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"URL POC" message:[NSString stringWithFormat:@"URL return from %@: %@", self.editText.text, [ViewController getCName]] preferredStyle:UIAlertControllerStyleAlert];
+    UIAlertAction *acknowledge = [UIAlertAction actionWithTitle:@"Acknowledge" style:UIAlertActionStyleDefault handler:^(UIAlertAction* _Nonnull action) {
         NSLog(@"User acknowledge");
     }];
     [alert addAction:acknowledge];
