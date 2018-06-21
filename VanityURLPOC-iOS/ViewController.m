6199e6002c2411f5e7c98b812d699db7fe829145
Hai-Dang Dam <haidang.dam@okta.com>
2018-06-21 13:39:12 -0700

    Adding initial code

--- VanityURLPOC-iOS/ViewController.m
+++ VanityURLPOC-iOS/ViewController.m
@@ -8,13 +8,18 @@
 
 #import "ViewController.h"
 #import <netdb.h>
+#import <dns_sd.h>
 
 @interface ViewController ()
+
 @property (weak, nonatomic) IBOutlet UITextField *editText;
 
 @end
 
+static ViewController *viewController;
+
 @implementation ViewController
+
 
 - (NSString *) canonicalAddressForHostAddress: (NSString *)hostAddess {
     const char* hostAddress_char = [hostAddess UTF8String];
@@ -22,25 +27,39 @@
     int retval;
     
     memset(&hints, 0, sizeof(struct addrinfo));
-    hints.ai_family = AF_UNSPEC;
+    hints.ai_family = AF_INET;
     hints.ai_flags = AI_CANONNAME;
     
     retval = getaddrinfo(hostAddress_char, NULL, &hints, &res);
+    
+    
     if (retval == 0 && res->ai_canonname) {
         return [NSString stringWithUTF8String:res->ai_canonname];
     }
     return nil;
 }
 
-- (IBAction)touchButton:(UIButton *)sender {
-    NSString *userInput = self.editText.text;
-    NSString *canAddr = [self canonicalAddressForHostAddress:userInput];
-    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"URL POC" message:[NSString stringWithFormat:@"URL return from %@: %@", self.editText.text, canAddr] preferredStyle:UIAlertControllerStyleAlert];
+
+
+
+
+
+     
+- (void) showUserAlertWithString: (NSString *)string {
+    UIAlertController *alert = [UIAlertController alertControllerWithTitle:@"URL POC" message:[NSString stringWithFormat:@"URL return from %@: %@", self.editText.text, string] preferredStyle:UIAlertControllerStyleAlert];
     UIAlertAction *acknowledge = [UIAlertAction actionWithTitle:@"Acknowledge" style:UIAlertActionStyleDefault handler:^(UIAlertAction * _Nonnull action) {
         NSLog(@"User acknowledge");
     }];
     [alert addAction:acknowledge];
     [self presentViewController:alert animated:YES completion:nil];
+}
+
+
+
+- (IBAction)touchButton:(UIButton *)sender {
+    NSString *userInput = self.editText.text;
+    NSString *canAddr = [self canonicalAddressForHostAddress:userInput];
+    [self showUserAlertWithString:canAddr];
 }
 
 
